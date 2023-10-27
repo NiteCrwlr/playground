f0591fd451bb028da103ca1e80dcfe71244c4475
NiteCrwlr <55795203+NiteCrwlr@users.noreply.github.com>
2023-10-27 15:16:23 +0200

    Catch error if HomeAssistant is not reachable and a bit of cleanup.

--- SNStatus/SNStatus.py
+++ SNStatus/SNStatus.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# NEEDS: sudo pip3 install requests
+# Requires: sudo pip3 install requests
+#
 import socket
 import requests
 import json
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-haToken = "MY-HA-TOKEN"
+haToken = "MY-HA-API-TOKEN"                                 # Set your HomeAssistant API Token
+whUrl = 'https://X.X.X.X:8123/api/webhook/wh-snapmakera350' # Set to your HomeAssistant WebHook URL
 bufferSize = 1024
 msg = b'discover'
 destPort = 20054
@@ -14,8 +16,8 @@ sockTimeout = 1.0
 retries = 5
 retryCounter = 0
 snReply = {}
-whUrl = 'https://X.X.X.X:8123/api/webhook/wh-snapmakera350'
 
+# Main Program
 def main():
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDPClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -24,12 +26,15 @@ def main():
     checkState(UDPClientSocket,msg,destPort,retries)
     postIt(snReply)
 
+# Check status of Snapmaker 2.0
+# Possible replies:
+#  'Snapmaker@X.X.X.X|model:Snapmaker 2 Model A350|status:IDLE'
+#  'Snapmaker@X.X.X.X|model:Snapmaker 2 Model A350|status:RUNNING'
 def checkState(UDPClientSocket,msg,destPort,retries):
     global snReply
     global retryCounter
     UDPClientSocket.sendto(msg, ("255.255.255.255", destPort))
     try:
-        #'Snapmaker@X.X.X.X|model:Snapmaker 2 Model A350|status:IDLE'
         reply, server_address_info = UDPClientSocket.recvfrom(1024)
         elements = str(reply).split('|')
         snIP = (elements[0]).replace('\'','')
@@ -41,7 +46,6 @@ def checkState(UDPClientSocket,msg,destPort,retries):
         snReply = {"ip":snIPVal, "model":snModelVal, "state":snStatusVal}
     except socket.timeout:
         retryCounter += 1
-        #print("Timeout, retry",retryCounter)
         if (retryCounter==retries): 
           snReply = {"ip":"N/A", "model":"N/A", "state":"OFFLINE"}
           return
@@ -53,9 +57,11 @@ def postIt(state):
   session = requests.Session()
   session.verify = False
   print("Sending State: ", state)
-  requests.post(whUrl, json = state, verify=False)
+  try:
+      requests.post(whUrl, json = state, verify=False)
+  except requests.exceptions.ConnectionError:
+      print("Could not connect to HomeAssistant on", whUrl)
+      return
 
-
-
-
+# Run Main Program
 main()
